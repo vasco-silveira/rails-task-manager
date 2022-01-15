0cd63bc52e3cf93548a07def1e5de2618a07a580
Vasco Silveira <vasco.silveira@saltpay.co>
2022-01-15 15:07:29 +0000

    Added delete function and links to show, edit, and delete with a confirmation box for the latter

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,6 +28,9 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # CRUD
   # verb 'path', to: 'controller#action'
+
   # Fetch all tasks
   get '/tasks', to: 'tasks#index'
+
   # Fetch one task
   get "tasks/new", to: "tasks#new" # display the form
   get "tasks/:id", to: "tasks#show", as: :task
+
   # Create a task
   post "tasks", to: "tasks#create"
+
   # Update a task
-  get "tasks/:id/edit", to: "tasks#edit"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
   patch "tasks/:id", to: "tasks#update"
+
+  # Delete a task
+  delete "tasks/:id", to: "tasks#destroy"
 end
