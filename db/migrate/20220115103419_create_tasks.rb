bb178db733250cf910fba2e4f39c8f650556d324
Vasco Silveira <vasco.silveira@saltpay.co>
2022-01-15 11:31:00 +0000

    Add Task model

--- db/migrate/20220115103419_create_tasks.rb
+++ db/migrate/20220115103419_create_tasks.rb
@@ -2,8 +2,8 @@ class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.string :details
-      t.boolean :completed
+      t.text :details
+      t.boolean :completed, default: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema.define(version: 2022_01_15_103419) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.string "details"
-    t.boolean "completed"
+    t.text "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
